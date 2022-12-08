829b93183a5651567e5aded51ba93f3cf35f262b
Gabriel Bowen <gabe.bowen@utah.edu>
2022-12-08 07:48:37 -0700

    Update plots

--- code/plots.R
+++ code/plots.R
@@ -185,14 +185,8 @@ dev.off()
 ## Make space
 mod.p = double()
 
-## Calculate the CDF and find quantile value of modern median in it
-modCO2 = 417.93
-for(j in 1:length(ages)){
-  cdf = ecdf(exp(cp[, j]))
-  mod.p[j] = cdf(modCO2)
-}
-
-mod.pp = 1 - mod.p
+# Approach 1, using modern value
+modCO2 = 418
 
 ## Number of curves exceeding
 curve.p = double()
@@ -206,8 +200,7 @@ for(i in 1:ncol(cp.ex)){
   curve.p[i] = sum(ct) / nrow(cp.ex)
 }
 
-
-png("out/modprob.png", width = 9, height = 5, units = "in", 
+png("out/modprob1.png", width = 9, height = 5, units = "in", 
     res = 600)
 par(mai = c(1.1, 1.1, 0.1, 0.8))
 plot(0, 0, xlim = c(16, 0), ylim = c(100, 600), type = "n",
@@ -219,7 +212,7 @@ for(i in 1:250){
 }
 axis(1)
 axis(2, at = c(200, 300, 400, 500, 600))
-mtext(expression("CO"[2]*" (ppmv)"), 2, line = 3, at = 400)
+mtext(expression("CO"[2]*" (ppm)"), 2, line = 3, at = 400)
 
 par(new = TRUE)
 
@@ -230,6 +223,52 @@ axis(4, c(0.0001, 0.05, 0.5) ^ (1/3),
      c(expression("10"^{-4}), 0.05, 0.5), pos = 0)
 mtext("P(Exceedance)", 4, line = 2, at = 0.05 ^ (1/3))
 dev.off()
+
+# Approach 2, using change from preindustrial
+cp.anom = cp
+for(i in 1:nrow(cp)){
+  cp.anom[i,] = exp(cp[i,]) - exp(cp[i, ncol(cp)])
+}
+ 
+modanom = 418 - 280
+
+## Number of curves exceeding
+curve.p = double()
+cp.ex = cp.anom > modanom
+for(i in 1:ncol(cp.ex)){
+  if(ncol(cp.ex) - i > 0){
+    ct = apply(cp.ex[, i:ncol(cp.ex)], 1, any)
+  }else{
+    ct = any(cp.ex[, ncol(cp.ex)])
+  }
+  curve.p[i] = sum(ct) / nrow(cp.ex)
+}
+
+png("out/modprob2.png", width = 9, height = 5, units = "in", 
+    res = 600)
+par(mai = c(1.1, 1.1, 0.1, 0.8))
+plot(0, 0, xlim = c(16, 0), ylim = c(-150, 350), type = "n",
+     axes = FALSE, xlab = "Age (Ma)", ylab = "")
+lines(c(30, 0), rep(modanom, 2), col = "red", lty = 2, lwd = 2)
+for(i in 1:250){
+  lines(ages, cp.anom[runif(1, 1, nrow(cp.anom)),], 
+        col = rgb(0, 0, 0, 0.05), lwd=2)
+}
+axis(1)
+axis(2, at = c(0, 100, 200, 300))
+mtext(expression("CO"[2]*" change (ppm)"), 2, line = 3, at = 150)
+
+par(new = TRUE)
+
+plot(ages, curve.p ^ (1/3), type = "l", axes = FALSE, lwd = 2,
+     ylim=c(0, 2), xlim=c(16,0), xlab = "", ylab = "")
+lines(c(30, 0), rep(0.05 ^ (1/3), 2), col = "red", lty = 3, lwd = 2)
+axis(4, c(0.0001, 0.05, 0.5) ^ (1/3),
+     c(expression("10"^{-4}), 0.05, 0.5), pos = 0)
+mtext("P(Exceedance)", 4, line = 2, at = 0.05 ^ (1/3))
+dev.off()
+
+
 
 
 # version for talks
